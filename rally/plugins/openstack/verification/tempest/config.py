1e2ec065381c088cf559ab1152253ab8acde2b27
Dmitry Galkin <galkindmitrii@gmail.com>
2018-05-08 11:57:06 +0200

    Revert "Define designate and keystone as available services for now"
    This reverts commit 7b90da08191c3768e28c6c5a41180f25129eaa82.

--- rally/plugins/openstack/verification/tempest/config.py
+++ rally/plugins/openstack/verification/tempest/config.py
@@ -35,8 +35,7 @@ class TempestConfigfileManager(object):
     def __init__(self, deployment):
         self.credential = deployment.get_credentials_for("openstack")["admin"]
         self.clients = self.credential.clients()
-#        self.available_services = self.clients.services().values()
-        self.available_services = ["keystone", "designate"]
+        self.available_services = self.clients.services().values()
 
         self.conf = configparser.ConfigParser()
 
